929ead196e0f7a235b819e884b94fbf2da45db2f
Yudelka Torres Amparo <yudelka_torres@ucne.edu.do>
2026-01-18 00:11:36 -0400

    feat(dbcontext): add ApplicationDbContext with EF Core mappings.

--- GestionEntradasInventario/Data/ApplicationDbContext.cs
+++ GestionEntradasInventario/Data/ApplicationDbContext.cs
@@ -1,9 +1,28 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using GestionEntradasInventario.Models;
 
-namespace GestionEntradasInventario.Data
+namespace GestionEntradasInventario.Data;
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
 {
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
-    {
-    }
+   public DbSet<Productos> Productos { get; set; }
+   public DbSet<Entradas> Entradas { get; set; }
+   public DbSet<EntradasDetalle> EntradasDetalle { get; set; }
+
+   protected override void OnModelCreating(ModelBuilder builder)
+   {
+       base.OnModelCreating(builder);
+
+       builder.Entity<EntradasDetalle>()
+                .HasOne<Entradas>()       
+                .WithMany(e => e.EntradasDetalle)
+                .HasForeignKey(d => d.EntradaId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+      builder.Entity<EntradasDetalle>()
+                .HasOne<Productos>()        
+                .WithMany()
+                .HasForeignKey(d => d.ProductoId)
+                .OnDelete(DeleteBehavior.Restrict);
+   }
 }
